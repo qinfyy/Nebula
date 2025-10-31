e9f991355a08020e90ccf66a9537b62a6e72c199
Melledy <121644117+Melledy@users.noreply.github.com>
2025-10-31 07:26:46 -0700

    Update data version

--- src/main/java/emu/nebula/GameConstants.java
+++ src/main/java/emu/nebula/GameConstants.java
@@ -1,7 +1,7 @@
 package emu.nebula;
 
 public class GameConstants {
-    public static final int DATA_VERSION = 28;
+    public static final int DATA_VERSION = 31;
     public static final String VERSION = "1.0.0." + DATA_VERSION;
     
     public static final String PROTO_BASE_TYPE_URL = "type.googleapis.com/proto.";
